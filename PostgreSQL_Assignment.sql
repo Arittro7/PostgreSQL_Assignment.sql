c1e73bb200189cffea3d9e7b2ff61e91348b552c
Arittro7 <arittrowpdev@gmail.com>
2025-05-25 20:28:39 +0600

    tables

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,3 +1,18 @@
-create DATABASE test
+CREATE DATABASE conservation_db;
 
-DROP DATABASE test
+-- creating table rangers
+CREATE Table rangers(
+  ranger_id SERIAL PRIMARY KEY,
+  name VARCHAR(150),
+  region VARCHAR(150)
+)
+
+-- creating species table
+CREATE TABLE species(
+  species_id SERIAL PRIMARY KEY,
+  common_name VARCHAR(150),
+  scientific_name VARCHAR(150),
+  discovery_date DATE,
+  conservation_status VARCHAR(80)
+)
+
